3ddfac9b92e00cd332e21eeabe4d51fde27148bb
POWFIX <powfix@gmail.com>
2024-01-15 15:13:21 +0900

    정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {TodoItem} from "./components/todo/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    items: [],
+  };
+
+  componentDidMount() {
+    this.fetchTodos();
+  }
+
+  fetchTodos = () => {
+    setTimeout(() => {
+      this.setState({
+        items: [
+          {
+            title: '오늘 할일',
+            description: '오늘 할일 설명',
+          },
+        ]
+      })
+    }, 1000);
+
+    setTimeout(() => {
+      this.setState({
+        items: [
+          {
+            title: '오늘 할일',
+            description: '오늘 할일 설명',
+          },
+          {
+            title: '내일 할일',
+            description: '내일 할일 설명',
+          },
+        ]
+      })
+    }, 2000);
+
+    setTimeout(() => {
+      this.setState({
+        items: [
+          {
+            title: '오늘 할일',
+            description: '오늘 할일 설명',
+          },
+          {
+            title: '내일 할일',
+            description: '내일 할일 설명',
+          },
+          {
+            title: '모레 할일',
+            description: '모레 할일 설명',
+          }
+        ]
+      })
+    }, 3000);
+  };
+
+
+  render() {
+    return (
+      <div className="App" style={{display: 'flex', flexDirection: 'column', width: '100%', height: '100%'}}>
+        <div className={'todo'}>
+          {
+            this.state.items.map((item) => {
+              return (
+                <TodoItem title={item.title} description={item.description} />
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
